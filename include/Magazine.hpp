f6c57e2b36674f57f5fbc2e19bc3131228c1d74e
Rahman Md Naimur <naimurrahman2104@gmail.com>
2025-12-09 20:25:23 +0900

    Update Magazine.hpp

--- include/Magazine.hpp
+++ include/Magazine.hpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 class Magazine : public Item {
-    int issueNumber;
+    int issueNumber;  // issue number
 
 public:
-    Magazine(int id, string title, int issue, int count);
-    void display() const override;
-    string serialize() const override;
+    Magazine(int id, string title, int issue, int count); // make a magazine
+    void display() const override; // show magazine info
+    string serialize() const override;  // save magazine as text
 };
 
 #endif
